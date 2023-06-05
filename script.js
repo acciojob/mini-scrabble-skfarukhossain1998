22ba244ce005a189785e20d08c7202ef907659b3
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-06-05 12:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+const textBox=document.getElementById("evaluatedText");
+const tag=document.getElementById("letterCount");
+
+textBox.addEventListener('input',countNumber);
+function countNumber() {
+	const textValue=textBox.value;
+	const length=textValue.length;
+	tag.textContent =length;
+}
+
+countNumber();
